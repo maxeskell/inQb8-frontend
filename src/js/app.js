fe96eddbd68e69347c37ea8500b81501da068a1a
maxeskell <maxeskell1@gmail.com>
2017-07-26 16:05:14 +0100

    Updated form validation

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,3 @@
 angular
-  .module('incubate', ['ui.router', 'ngResource', 'satellizer', 'checklist-model'])
+  .module('incubate', ['ui.router', 'ngResource', 'satellizer', 'checklist-model', 'ngMessages'])
   .constant('API_URL', 'http://localhost:3000/api');
