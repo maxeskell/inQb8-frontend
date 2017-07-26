2d8d125353baeacffb71eebc543f133f38b473c8
maxeskell <maxeskell1@gmail.com>
2017-07-26 09:57:17 +0100

    facebook attempt to fix 1

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,3 @@
 angular
   .module('incubate', ['ui.router', 'ngResource', 'satellizer', 'checklist-model'])
-  .constant('API_URL', 'https://incubateapi.herokuapp.com');
+  .constant('API_URL', 'https://incubateapi.herokuapp.com/app');
